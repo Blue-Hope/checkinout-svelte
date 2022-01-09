d59f0a80c151a252f18676d6065590ca2c01bda3
blue-hope <ms7045436@likelion.org>
2022-01-10 07:29:28 +0900

    Add firebase config

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -1,13 +1,16 @@
 import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDbNw4maw6xbxCoJb0tmroOLWkDeJoEVlE",
-  authDomain: "fakmanage.firebaseapp.com",
-  projectId: "fakmanage",
-  storageBucket: "fakmanage.appspot.com",
-  messagingSenderId: "354672172189",
-  appId: "1:354672172189:web:b89ea9f57d9410e7ea0874",
-};
+export function initializeFirebase() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyDbNw4maw6xbxCoJb0tmroOLWkDeJoEVlE",
+    authDomain: "fakmanage.firebaseapp.com",
+    projectId: "fakmanage",
+    storageBucket: "fakmanage.appspot.com",
+    messagingSenderId: "354672172189",
+    appId: "1:354672172189:web:b89ea9f57d9410e7ea0874",
+    databaseURL:
+      "https://fakmanage-default-rtdb.asia-southeast1.firebasedatabase.app",
+  };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+  return initializeApp(firebaseConfig);
+}
